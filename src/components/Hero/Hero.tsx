76b8f9ddae2e8579c52ae10e13a20e56a30ac7cf
Herbert-Ken Ymera <hkymera2@gmail.com>
2024-03-01 01:45:06 +0200

    added an animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { getImageUrl } from '../../utils'
+import { getImageUrl } from '../../utils';
+import style from './Hero.module.scss';
+
 
 export default function Hero():JSX.Element {
   return (<section>
-    <div>
+    <div className={style.content}>
         <h1>Hello! I am Herbert!</h1>
         <p>I am a full stack software engineer/developer, with 4 years of experience.</p>
-        <a href="mailto:hkymera2@gmail.com">Email you can reach me on.</a>
+        <a href="mailto:hkymera2@gmail.com">Contact email</a>
     </div>
     <img src={getImageUrl("hero/heroImage.png")} alt="Image of me" />
+    <div className={style.topBlur}></div>
+    <div className={style.bottomBlur}></div>
   </section>)
 }
