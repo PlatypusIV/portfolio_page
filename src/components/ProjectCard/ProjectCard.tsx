9e6d79763725506efe97e31ffa92c2b8977ddb20
Herbert-Ken Ymera <hkymera2@gmail.com>
2024-03-11 01:16:19 +0200

    added projects.json

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { getImageUrl } from '../../utils';
 
-export default function ProjectCard():JSX.Element {
+interface Props {
+  imageSource: string;
+  title: string;
+  description: string;
+  skills: string[];
+  demo: string;
+  source: string;
+  id: number;
+}
+
+export default function ProjectCard(props: Props):JSX.Element {
+  const {imageSource, title, description, skills, demo, source, id} = props;
+
   return (
-    <div>ProjectCard</div>
+    <div key={id}>
+              <img src={getImageUrl(imageSource)} alt={`Image of ${title}`} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+            <ul>{skills.map((skill, id)=>
+              <li key={id}>{skill}</li>
+            )}
+            </ul>
+            <div>
+              <a href={demo}>Demo</a>
+              <a href={source}>Source</a>
+            </div>
+            </div>
   )
 }
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import style from "./Projects.module.scss";
+import projects from "../../data/projects.json";
+import { getImageUrl } from '../../utils';
+
 
 export default function Projects():JSX.Element {
   return (
     <section id="projects">
-
+      <h2>
+        <div>
+          {projects.map((project, id)=>{
+            return 
+          })}
+        </div>
+      </h2>
     </section>
   )
 }
