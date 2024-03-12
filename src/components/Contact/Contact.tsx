7c3cf13dc8fd6ef9cf17f0e7718af62ec2e6aa10
Herbert-Ken Ymera <hkymera2@gmail.com>
2024-03-13 01:50:54 +0200

    added contact page, need to fix anchor tags

---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import style from './Contact.module.scss';
+import { getImageUrl } from '../../utils';
+
+export default function Contact():JSX.Element {
+  return (
+    <footer id="contact" className={style.container}>
+        <div className={style.text}>
+            <h2>Contact</h2>
+            <p>Feel free to contact me!</p>
+        </div>
+        <ul className={style.links}>
+            <li>
+                <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                <a href="mailto:hkymera2@gmail.com">hkymera2@gmail.com</a>
+            </li>
+            <li>
+                <img src={getImageUrl("contact/linkedinIcon.png")} alt="Linkedin icon" />
+                <a href="https://www.linkedin.com/in/herbert-ken-ümera">linkedin.com/herbert-ken-ümera</a>
+            </li>
+            <li>
+                <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+                <a href="https://github.com/PlatypusIV">github.com/PlatypusIV</a>
+            </li>
+        </ul>
+    </footer>
+  )
+}
