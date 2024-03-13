aa7c62ba2ed66c5442e00dd1f94e13c405c46e9c
Herbert-Ken Ymera <hkymera2@gmail.com>
2024-03-14 00:42:11 +0200

    added some proper images for exp section. Need to rewrite whole about section to look nicer

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { getImageUrl } from '../../utils';
+// import { getImageUrl } from '../../utils';
 import experience from "../../data/experience.json";
 import ExperienceBox from '../ExperienceBox/ExperienceBox';
 import style from './About.module.scss';
@@ -10,11 +10,11 @@ export default function About():JSX.Element {
     <section className={style.container} id="about">
         <h2 className={style.title}>About</h2>
         <div className={style.content}>
-            <img src={getImageUrl('about/aboutImage.png')} alt="Me researching some esoteric technologies" className={style.aboutImage} />
+            {/* <img src={getImageUrl('about/aboutImage.png')} alt="Me researching some esoteric technologies" className={style.aboutImage} /> */ /*need to find an image with me typing away on a computer */} 
             <ul>
-              <ExperienceBox title={experience.frontend.title} content={experience.frontend.content} imageUri="about/cursorIcon.png"/>
-              <ExperienceBox title={experience.fullstack.title} content={experience.fullstack.content} imageUri="about/uiIcon.png"/>
-              <ExperienceBox title={experience.gamedev.title} content={experience.gamedev.content} imageUri="about/cursorIcon.png"/>
+              <ExperienceBox title={experience.frontend.title} content={experience.frontend.content} imageUri="about/navigationIcon.png"/>
+              <ExperienceBox title={experience.fullstack.title} content={experience.fullstack.content} imageUri="about/serverIcon.png"/>
+              <ExperienceBox title={experience.gamedev.title} content={experience.gamedev.content} imageUri="about/gameControllerIcon.png"/>
             </ul>
         </div>
     </section>
--- src/components/ExperienceBox/ExperienceBox.tsx
+++ src/components/ExperienceBox/ExperienceBox.tsx
@@ -13,7 +13,7 @@ export default function ExperienceBox(props: Props):JSX.Element {
   const { title, content, imageUri, imageAlt } = props;
   return (
     <li className={style.experienceBox}>
-        <img src={getImageUrl(imageUri)}  alt={imageAlt ? imageAlt : "CursorIcon"}/>
+        <img src={getImageUrl(imageUri)}  alt={imageAlt ? imageAlt :`${title}-icon`}/>
         <div>
           <h3>{title}</h3>
           <p>{content}</p>
