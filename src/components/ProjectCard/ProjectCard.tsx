5ce07952c261ea4b59de4ea73af8247a455397a4
Herbert-Ken Ymera <hkymera2@gmail.com>
2024-06-28 16:18:46 +0300

    added github links

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -12,6 +12,7 @@ interface Project {
   description: string;
   skills: string[];
   source: string;
+  github: string;
 }
 
 export default function ProjectCard(props: Props):JSX.Element {
@@ -19,16 +20,19 @@ export default function ProjectCard(props: Props):JSX.Element {
   return (
     <div className={style.container}>
       <img src={getImageUrl(imageSource)} alt={`Image of ${title}`} className={style.image}/>
+      
       <div className={style.textContainer}>
         <h3 className={style.title}>{title}</h3>
         <p className={style.description}>{description}</p>
       </div>
+
       <ul className={style.skills}>{skills.map((skill, id)=>
         <li key={id}>{skill}</li>
       )}
       </ul>
       <div className={style.links}>
         <a href={source}>Project source</a>
+        <a href={source}>Github</a>
       </div>
     </div>
   )
